104b3a1ff8a95301be1b6acd77e1693ce2b98e67
Alex-Buttielie <63355403+Alex-Buttielie@users.noreply.github.com>
2023-09-28 17:54:07 -0300

    init

---
+++ src/main/java/br/com/coreeduc/coreeduc/CoreEducApplication.java
@@ -0,0 +1,13 @@
+package br.com.coreeduc.coreeduc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreEducApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreEducApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/coreeduc/coreeduc/CoreEducApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.coreeduc.coreeduc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreEducApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
