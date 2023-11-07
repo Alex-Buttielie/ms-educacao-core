6d4e4d66b9071eaee24438237f761f9423a5deb1
Alex-Buttielie <63355403+Alex-Buttielie@users.noreply.github.com>
2023-11-07 17:38:13 -0300

    Criação das entidades referente ao Calendario
    Criação das entidades referente ao Calendario

--- src/main/resources/db/migration/V47__create-table_calendario.sql
+++ src/main/resources/db/migration/V47__create-table_calendario.sql
@@ -2,8 +2,8 @@ CREATE SEQUENCE CALENDARIO_ID_SEQ;
 
 CREATE TABLE IF  NOT EXISTS CALENDARIO (id INTEGER UNIQUE DEFAULT nextval('CALENDARIO_ID_SEQ'),
                                         nome varchar (200),
-                                        fk_id_ano INTEGER,
-                                        CONSTRAINT fk_ano FOREIGN KEY (fk_id_ano)
+                                        ano_id INTEGER,
+                                        CONSTRAINT fk_ano FOREIGN KEY (ano_id)
                                             REFERENCES ANO (id)
 );
 
