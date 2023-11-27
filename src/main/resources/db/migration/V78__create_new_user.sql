0ac0ed726b556a462c4dbdff53239da0d71dfa82
Alex-Buttielie <63355403+Alex-Buttielie@users.noreply.github.com>
2023-11-27 18:14:05 -0300

    melhoria

--- src/main/resources/db/migration/V78__create_new_user.sql
+++ src/main/resources/db/migration/V78__create_new_user.sql
@@ -10,6 +10,6 @@ insert into TB_USER (ds_login,
                      sex,
                      user_typer, tenant)
 
-values ('Alex', 'Alex Buttielie', '70846792198',
-        '$2a$10$3XhhrCOssmvwSty2jH2CveJFyaybagNXzND2WC0SPFGcROvZt5ze.', '6796001', 'buttielle@gmail.com', '2001-03-16',
-        '64999077331', now(), 1, 1, 'tenant_2');
+values ('SECRE', 'Secretario', '93140441061',
+        '$2a$10$3XhhrCOssmvwSty2jH2CveJFyaybagNXzND2WC0SPFGcROvZt5ze.', '145875647', 'secretario@gmail.com', '2001-03-16',
+        '62999077331', now(), 1, 1, 'tenant_2');
